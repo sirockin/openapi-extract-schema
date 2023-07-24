b2ca43e680ad8a4f54da0d79896001b5089b08c7
Dave Sirockin <dave@banked.com>
2023-07-24 10:20:49 +0200

    chore: create boilerplate

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sirockin/openapi-extract-schema
+
+go 1.20
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: openapi-extract-schema {input-file} {output-file}")
+		return
+	}
+
+	inputFileName := os.Args[1]
+	outputFileName := os.Args[2]
+
+	inStream, err := os.Open(inputFileName)
+	if err != nil {
+		panic(err)
+	}
+	outStream, err := os.Open(outputFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	spec := map[string]interface{}{}
+	err = yaml.NewDecoder(inStream).Decode(&spec)
+	if err != nil {
+		panic(err)
+	}
+
+	outSpec := transform(spec)
+	yaml.NewEncoder(outStream).Encode(outSpec)
+}
+
+func transform(in map[string]interface{})map[string]interface{}{
+	return in
+}
